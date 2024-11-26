fce41e78e85299028559cb498445d7b19fece593
leohenrique0912 <0000107480823xsp@al.educacao.sp.gov.br>
2024-11-26 17:00:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Corinthians',
+    'Qual é o mascote do Corinthians?',
+    'O mascote do Corinthians é o "Mosqueteiro"'
+)
+
+criaCartao(
+    'Corinthians',
+    'Quem é o maior ídolo da história do Corinthians?',
+    'O maior ídolo da história do Corinthians é o Ronaldo Fenômeno'
+)
+
+criaCartao(
+    'Corinthians',
+    'Qual o estádio do Corinthians?',
+    'O estádio do Corinthians é a Arena Corinthians, localizada em Itaquera, São Paulo'
+)
+
+criaCartao(
+    'Corinthians',
+    'Qual foi o maior título conquistado pelo Corinthians?',
+    'O maior título conquistado pelo Corinthians foi o Mundial de Clubes da FIFA, em 2000 e 2012'
+)
